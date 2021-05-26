fccff44dc838c6046bcc30942cc6e6a240b3b98f
ducnv <ducnv@tima.vn>
2021-05-26 13:54:19 +0700

    update

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ cron.schedule(`*/${TIME_OVER} * * * * *`, () => {
     console.log(new Date().getTime()-global.lastPing)
     if((new Date().getTime() - global.lastPing)>TIME_OVER){
         try{
-            axios.post('https://hooks.slack.com/services/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', {text: 'Cảnh báo dịch vụ đọc tin nhắn'})
+            axios.post('https://hooks.slack.com/services/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', {text: 'Cảnh báo dịch vụ đọc tin nhắn'})
                 .then(function (response) {
                     // handle success
                     console.log(response.status);
